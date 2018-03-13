bb809bb53f16b4b55ea7c38210237251d6b4bc9d
Arthur Lazarenko <sasha.bobcko@gmail.com>
2018-03-13 12:20:35 +0300

    Законено переписывание проекта с GOlang.

--- core/brain.js
+++ core/brain.js
@@ -1,86 +1,155 @@
 let brain = require("brain.js"),
-    utilities = require("./utilities.js");
+  utilities = require("./utilities.js");
 
 const config = {
-    paths: {
-        snapshot: "neuralnetwork.snapshot",
-        dictionary: "dialogs.dictionary",
-        initialDialogs: "./train/initial.dialogs",
-        encodedDialogs: "./train/encoded.dialogs"
-    },
+  paths: {
+    snapshot: "neuralnetwork.snapshot",
+    dictionary: "dialogs.dictionary",
+    dialogs: "initial.dialogs"
+  },
 
-    network: {
-        inputs: 50,
-        hiddens: 50,
-        outputs: 50
-    }
+  words: {
+    inputs: 50,
+    outputs: 50
+  },
+
+  network: {
+    activation: "sigmoid",
+    hiddenLayers: [100],
+    learningRate: 0.3
+  },
+
+  train: {
+    iterations: 200000,
+    errorThresh: 0.005,
+    log: true,
+    logPeriod: 10,
+    learningRate: 0.3,
+    momentum: 0.1,
+    callback: null,
+    callbackPeriod: 10,
+    timeout: Infinity
+  }
 };
 
 module.exports.initialize = async () => {
-    try {
-        let neuralNetwork = new brain.recurrent.LSTM();
+  try {
+    let neuralNetwork = new brain.recurrent.LSTM(config.network);
 
-        if (await utilities.checkExistenceFile(config.paths.snapshot)) {
-            neuralNetwork.fromJSON(
-                JSON.parse(
-                    await utilities.readFromFile(config.paths.snapshot, false)
-                )
-            );
-        } else {
-            console.log("Beginning of neural network training.");
+    if (await utilities.checkExistenceFile(config.paths.snapshot)) {
+      neuralNetwork.fromJSON(
+        JSON.parse(await utilities.readFromFile(config.paths.snapshot, false))
+      );
+    } else {
+      console.log("Beginning of neural network training.");
 
-            await createDictionary();
+      await createDictionary();
 
-            console.log("The neural network training has been completed.");
-        }
+      await neuralNetwork.train(await createPatternsForTrains(), config.train);
 
-        return {
-            network: neuralNetwork,
-            dictionary: JSON.parse(
-                await utilities.readFromFile(config.paths.dictionary, true)
-            )
-        };
-    } catch (error) {
-        console.error(
-            "Failed to initialize neural network. " + Error(error).message
-        );
+      console.log("The neural network training has been completed.");
     }
+
+    return {
+      network: neuralNetwork,
+      dictionary: JSON.parse(
+        await utilities.readFromFile(config.paths.dictionary, true)
+      )
+    };
+  } catch (error) {
+    console.error(
+      "Failed to initialize neural network. " + Error(error).message
+    );
+  }
 };
 
 module.exports.completion = async neuralNetwork => {
-    try {
-        await utilities.writeToFile(
-            config.paths.snapshot,
-            neuralNetwork.toJSON(),
-            true
-        );
-    } catch (error) {
-        console.error(
-            "Error of the neural network session termination. " +
-                Error(error).message
-        );
-    }
+  try {
+    await utilities.writeToFile(
+      config.paths.snapshot,
+      JSON.stringify(neuralNetwork.toJSON()),
+      true
+    );
+  } catch (error) {
+    console.error(
+      "Error of the neural network session termination. " + Error(error).message
+    );
+  }
+};
+
+module.exports.activate = async (string, neuralNetwork, dictionary) => {
+  return await utilities.codeArrayToString(
+    neuralNetwork.run(await utilities.stringToCodeArray(string, dictionary))
+  );
 };
 
 var createDictionary = async () => {
-    try {
-        dialogs = (await utilities.readFromFile(
-            config.paths.initialDialogs,
-            true
-        )).split("\r\n");
+  try {
+    dialogs = (await utilities.readFromFile(config.paths.dialogs, true)).split(
+      "\r\n"
+    );
 
-        var dictionary = {};
+    var dictionary = {};
 
-        await dialogs.forEach(async sentense => {
-            words = (await utilities.clearText(sentense)).split(" ");
+    for (countDialogs = 0; countDialogs < dialogs.length; countDialogs++) {
+      var words = (await utilities.clearText(dialogs[countDialogs])).split(" ");
 
-            await words.forEach(async word => {
-                dictionary[word] = (Math.random() * (0 - 1) + 1).toFixed(64);
-            });
-        });
+      for (wordsCount = 0; wordsCount < words.length; wordsCount++) {
+        dictionary[words[wordsCount]] = (Math.random() * (0 - 1) + 1).toFixed(
+          32
+        );
+      }
 
-        await utilities.writeToFile(config.paths.dictionary, dictionary, true);
-    } catch (error) {
-        console.error("Error creating dictionary. " + Error(error).message);
+      await words.forEach(async word => {});
     }
+
+    await utilities.writeToFile(
+      config.paths.dictionary,
+      JSON.stringify(dictionary),
+      true
+    );
+  } catch (error) {
+    console.error("Error creating dictionary. " + Error(error).message);
+  }
+};
+
+var createPatternsForTrains = async () => {
+  try {
+    var dictionary = JSON.parse(
+        await utilities.readFromFile(config.paths.dictionary, false)
+      ),
+      dialogs = (await utilities.readFromFile(
+        config.paths.dialogs,
+        true
+      )).split("\r\n"),
+      countDialogs = 0,
+      patterns = [];
+
+    for (
+      countPatterns = 0;
+      countPatterns < dialogs.length / 2;
+      countPatterns++
+    ) {
+      patterns.push({
+        input: await utilities.stringToCodeArray(
+          await utilities.clearText(dialogs[countDialogs]),
+          dictionary,
+          config.words.inputs
+        ),
+        output: await utilities.stringToCodeArray(
+          await utilities.clearText(dialogs[countDialogs + 1]),
+          dictionary,
+          config.words.outputs
+        )
+      });
+
+      return patterns;
+    }
+    console.log(patterns);
+    return patterns;
+  } catch (error) {
+    console.error(
+      "Could not create template for training. " + Error(error).message
+    );
+  }
 };
--- core/utilities.js
+++ core/utilities.js
@@ -1,102 +1,93 @@
 let fs = require("fs");
 
 module.exports.clearText = async string => {
-    try {
-        return string.trim().replace("[^ a-z A-Z а-я А-Я 0-9 , ! ? .]");
-    } catch (error) {
-        console.error("Unable to clear text. " + Error(error).message);
-    }
+  try {
+    return string.trim().replace("[^ a-z A-Z а-я А-Я 0-9 , ! ? .]");
+  } catch (error) {
+    console.error("Unable to clear text. " + Error(error).message);
+  }
 };
 
 module.exports.readFromFile = async (path, autoCreate) => {
-    try {
-        if (!fs.existsSync(path) && autoCreate) {
-            fs.writeFileSync(path, "", null, "utf8");
-        }
-        return fs.readFileSync(path, "utf8");
-    } catch (error) {
-        console.error(
-            "Could not read data from file [" +
-                path +
-                "]. " +
-                Error(error).message
-        );
+  try {
+    if (!fs.existsSync(path) && autoCreate) {
+      fs.writeFileSync(path, "", null, "utf8");
     }
+    return fs.readFileSync(path, "utf8");
+  } catch (error) {
+    console.error(
+      "Could not read data from file [" + path + "]. " + Error(error).message
+    );
+  }
 };
 
 module.exports.writeToFile = async (path, data, overwrite) => {
-    try {
-        fs.writeFileSync(
-            path,
-            JSON.stringify(data),
-            { flag: overwrite ? "w" : "a" },
-            "utf8"
-        );
-    } catch (error) {
-        console.error(
-            "Could not write to the file [" +
-                path +
-                "]. " +
-                Error(error).message
-        );
-    }
+  try {
+    fs.writeFileSync(path, data, { flag: overwrite ? "w" : "a" }, "utf8");
+  } catch (error) {
+    console.error(
+      "Could not write to the file [" + path + "]. " + Error(error).message
+    );
+  }
 };
 
 module.exports.checkExistenceFile = async path => {
-    try {
-        return fs.existsSync(path);
-    } catch (error) {
-        console.error(
-            "Could not determine the existence of the file [" +
-                path +
-                "]. " +
-                Error(error).message
-        );
-    }
+  try {
+    return fs.existsSync(path);
+  } catch (error) {
+    console.error(
+      "Could not determine the existence of the file [" +
+        path +
+        "]. " +
+        Error(error).message
+    );
+  }
 };
 
 module.exports.stringToCodeArray = async (string, dictionary, size) => {
-    try {
-        var words = (await this.clearText(string)).split(" ");
+  try {
+    var words = (await this.clearText(string)).split(" ");
 
-        var array = [];
+    var array = [];
 
-        await words.forEach(async (word, index) => {
-            if (index < size) {
-                array.push(dictionary.get(word));
-            }
-        });
-
-        for (count = array.length; count < size; count++) {
-            array.push(0);
-        }
-
-        return array;
-    } catch (error) {
-        console.error(
-            "Could not convert string to array of codes. " +
-                Error(error).message
-        );
+    for (count = 0; count < words.length; count++) {
+      if (count < size) {
+        array.push(dictionary[words[count]]);
+      }
     }
+
+    for (count = array.length; count < size; count++) {
+      array.push(0);
+    }
+
+    return array;
+  } catch (error) {
+    console.error(
+      "Could not convert string to array of codes. " + Error(error).message
+    );
+  }
 };
 
 module.exports.codeArrayToString = async (array, dictionary) => {
-    try {
-        let string = "";
+  try {
+    let string = "";
 
-        await array.forEach(async code => {
-            await dictionary.forEach(async (key, word) => {
-                if (key == code) {
-                    string += word + " ";
-                }
-            });
-        });
-
-        return await this.clearText(string);
-    } catch (error) {
-        console.error(
-            "Could not convert array of codes to string. " +
-                Error(error).message
-        );
+    for (arrayCount = 0; arrayCount < array.length; arrayCount++) {
+      for (
+        dictionaryCount = 0;
+        dictionaryCount < dictionary.length;
+        dictionaryCount++
+      ) {
+        if (dictionaryCount == array[arrayCount]) {
+          string += dictionary[dictionaryCount] + " ";
+        }
+      }
     }
+
+    return await this.clearText(string);
+  } catch (error) {
+    console.error(
+      "Could not convert array of codes to string. " + Error(error).message
+    );
+  }
 };
