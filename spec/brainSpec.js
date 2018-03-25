6dafe49dcce87a2a115fddc8b2324aece669ed73
Arthur Lazarenko <Arthur.Lazarenko@zoho.com>
2018-03-25 21:01:18 +0300

    Мелкие правки тестов.

--- spec/brainSpec.js
+++ spec/brainSpec.js
@@ -2,8 +2,8 @@ const brain = require('../core/brain');
 
 describe('brain', () => {
   it('createPatternsForTrains', () => {
-    const dialogs = ['\t Привет, как дела?', 'Привет, хорошо! ^', '', 'Завтра дует снег? \ |  ', 'Да, будет.  '];
-    const answer = [{ input: ['ПРИВЕТ~', 'КАК~', 'ДЕЛА~'], output: ['ПРИВЕТ~', 'ХОРОШО~'] }, { input: ['ЗАВТРА~', 'ДУЕТ~', 'СНЕГ~'], output: ['ДА~', 'БУДЕТ~'] }];
+    const dialogs = ['\t Привет, как дела?', 'Привет, хорошо! ^', '  ', 'Завтра будет снег? // |  ', 'Да, будет.  '];
+    const answer = [{ input: ['ПРИВЕТ~', 'КАК~', 'ДЕЛА~'], output: ['ПРИВЕТ~', 'ХОРОШО~'] }, { input: ['ЗАВТРА~', 'БУДЕТ~', 'СНЕГ~'], output: ['ДА~', 'БУДЕТ~'] }];
     expect(brain.createPatternsForTrains(dialogs)).toEqual(answer);
   });
 });
